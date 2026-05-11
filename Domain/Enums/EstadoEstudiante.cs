63d392d01dfa08e27862baec7cbf73eed12a6c9f
Cristaly R. <2019-0711@uteco.edu.do>
2026-05-10 21:19:36 -0400

    cambios

---
+++ Domain/Enums/EstadoEstudiante.cs
@@ -0,0 +1,16 @@
+namespace MiProyectoAcademico.Domain.Enums;
+
+public enum EstadoEstudiante
+{
+    Activo    = 1,
+    Inactivo  = 2,
+    Egresado  = 3,
+    Suspendido = 4
+}
+
+public enum TipoDocumento
+{
+    Cedula   = 1,
+    Pasaporte = 2,
+    Licencia  = 3
+}
