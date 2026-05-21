dc63fe78f40ab42337b9e844c5b5016e54d10133
Cristaly R. <2019-0711@uteco.edu.do>
2026-05-20 20:57:41 -0400

    Cambios y nuevas entidades

---
+++ Data/Migrations/20260521001653_AddTables.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LP3.BlazorServer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
